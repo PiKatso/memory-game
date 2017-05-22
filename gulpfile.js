8d8d0254e8b7d3ad5d93ace454f9e65db4c9d4d5
Kat Gifford and Tanner Eustice <tanner@>
2017-05-22 15:33:05 -0700

    implement live dev server

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,23 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
+var browserSync = require('browser-sync').create();
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
 var buildProduction = utilities.env.production;
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
 
 gulp.task('jsbrowserify', ['concatInterface'], function(){
   return browserify({ entries: ['./tmp/allConcat.js']})
@@ -44,4 +59,37 @@ gulp.task("build", ['clean', 'jshint'], function(){
   } else {
     gulp.start('jsbrowserify')
   }
+  gulp.start('bower');
+});
+
+gulp.task('bowerJS', function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'))
+});
+
+gulp.task('bowerCSS', function () {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+
+gulp.task('jsBuild', ['jsbrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
 });
